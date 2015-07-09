7a6c6a442afefd2de43eaa118663babaa4dd0c73
Chris Stewart <cstewart@bignerdranch.com>
2015-07-09 10:19:04 -0400

    Added RotateBehavior

---
+++ app/src/main/java/com/bignerdranch/android/custombehavior/RotateBehavior.java
@@ -0,0 +1,51 @@
+package com.bignerdranch.android.custombehavior;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.List;
+
+public class RotateBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    private static final String TAG = ShrinkBehavior.class.getSimpleName();
+
+    public RotateBehavior() {
+    }
+
+    public RotateBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        float translationY = getFabTranslationYForSnackbar(parent, child);
+        float percentComplete = -translationY / dependency.getHeight();
+        child.setRotation(-90 * percentComplete);
+        child.setTranslationY(translationY);
+        return false;
+    }
+
+    private float getFabTranslationYForSnackbar(CoordinatorLayout parent,
+                                                FloatingActionButton fab) {
+        float minOffset = 0;
+        final List<View> dependencies = parent.getDependencies(fab);
+        for (int i = 0, z = dependencies.size(); i < z; i++) {
+            final View view = dependencies.get(i);
+            if (view instanceof Snackbar.SnackbarLayout && parent.doViewsOverlap(fab, view)) {
+                minOffset = Math.min(minOffset,
+                        ViewCompat.getTranslationY(view) - view.getHeight());
+            }
+        }
+
+        return minOffset;
+    }
+}
